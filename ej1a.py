1ef1458907871a950ed4a21c2023dfd848ec5370
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2023-11-17 18:57:40 -0300

    feat: compare architectures graph

--- ej1a.py
+++ ej1a.py
@@ -5,6 +5,60 @@ from perceptron.functions import *
 from perceptron.multi_perceptron import *
 from perceptron.optimizers import ADAM, Momentum
 from training_data.font import *
+import plotly.graph_objects as go
+import numpy as np
+
+
+def compare_architectures():
+    architectures = config_json["architectures"]
+    optimizer = ADAM
+    optimizer_args = [
+        config_json["optimizer"]["alpha"],
+        config_json["optimizer"]["beta1"],
+        config_json["optimizer"]["beta2"],
+        config_json["optimizer"]["epsilon"]
+    ]
+    metrics = {}
+    for architecture in architectures:
+        architecture_name = str(architecture)
+        metrics[architecture_name] = []
+        for _ in range(config_json["runs"]):
+            autoencoder = MultiPerceptron(
+                architecture,
+                partial(sigmoid, config_json["activation_function_beta"]),
+                partial(sigmoid_derivative, config_json["activation_function_beta"]),
+                optimizer,
+                optimizer_args
+            )
+            error = autoencoder.train(config_json['architecture_epochs'], fonts, fonts, len(fonts))
+            metrics[architecture_name].append(error)
+
+    print(metrics)
+    architectures = list(metrics.keys())
+    average_errors = [round(np.mean(errors)) for errors in metrics.values()]
+    std_deviation = [round(np.std(errors)) for errors in metrics.values()]
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x=architectures,
+        y=average_errors,
+        error_y=dict(type='data', array=std_deviation, visible=True),
+        marker=dict(color='lightblue'),
+        textposition='outside',
+        texttemplate='%{y}',
+        name='Average Error'
+    ))
+
+    fig.update_layout(
+        title='Autoencoder Architectures Pixel Difference',
+        xaxis_title='Architectures',
+        yaxis_title='Pixel Difference'
+    )
+
+    fig.show()
+
+
 
 
 if __name__ == "__main__":
@@ -57,3 +111,6 @@ if __name__ == "__main__":
     print("\n- = - = - RUNNING TESTS - = - = -\n")
 
     autoencoder.test(fonts, fonts)
+
+    if config_json["compare_architectures"]:
+        compare_architectures()
