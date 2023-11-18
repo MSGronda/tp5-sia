c05317f8f35634a4fae11160927fa6031a45fc52
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2023-11-18 15:38:52 -0300

    feat: optimizers comparison

--- ej1a.py
+++ ej1a.py
@@ -51,15 +51,70 @@ def compare_architectures():
     ))
 
     fig.update_layout(
-        title='Autoencoder Architectures Pixel Difference',
+        title='Average MSE per Autoencoder Architecture',
         xaxis_title='Architectures',
-        yaxis_title='Pixel Difference'
+        yaxis_title='MSE'
     )
 
     fig.show()
 
+def compare_optimizers():
+    optimizers = [ADAM, Momentum]
+    architecture = config_json["optimizer_architecture"]
+    optimizer_args = [
+        [
+            config_json["optimizer"]["alpha"],
+            config_json["optimizer"]["beta1"],
+            config_json["optimizer"]["beta2"],
+            config_json["optimizer"]["epsilon"]
+        ],
+        [
+            config_json["momentum"]["learning_rate"],
+            config_json["momentum"]["alpha"]
+        ]
+    ]
 
+    metrics = {}
+    for i in range(len(optimizers)):
+        optimizer = optimizers[i]
+        args = optimizer_args[i]
 
+        optimizer_name = optimizer.name()
+        metrics[optimizer_name] = []
+        for _ in range(config_json["optimizer_runs"]):
+            autoencoder = MultiPerceptron(
+                architecture,
+                partial(sigmoid, config_json["activation_function_beta"]),
+                partial(sigmoid_derivative, config_json["activation_function_beta"]),
+                optimizer,
+                args
+            )
+            error = autoencoder.train(config_json["optimizer_limit"], fonts, fonts, len(fonts))
+            metrics[optimizer_name].append(error)
+
+    optimizers = list(metrics.keys())
+    average_errors = [np.mean(errors) for errors in metrics.values()]
+    std_deviation = [np.std(errors) for errors in metrics.values()]
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x=optimizers,
+        y=average_errors,
+        error_y=dict(type='data', array=std_deviation, visible=True),
+        marker=dict(color='lightblue'),
+        textposition='outside',
+        texttemplate='%{y}',
+        name='Average Error'
+    ))
+
+    fig.update_layout(
+        title='Average MSE per Optimizer',
+        xaxis_title='Optimizers',
+        yaxis_title='MSE'
+    )
+
+    fig.show()
 
 if __name__ == "__main__":
     with open("ej1a_config.json", "r") as f:
@@ -80,7 +135,8 @@ if __name__ == "__main__":
 
     elif config_json["optimizer"]["type"] == "momentum":
         optimizer = Momentum
-        optimizer_args = [config_json["optimizer"]["learning_rate"]]
+        optimizer_args = [config_json["momentum"]["learning_rate"],
+                          config_json["momentum"]["alpha"]]
 
     else:
         RuntimeError("Invalid optimizer")
@@ -114,3 +170,6 @@ if __name__ == "__main__":
 
     if config_json["compare_architectures"]:
         compare_architectures()
+
+    if config_json["compare_optimizers"]:
+        compare_optimizers()
