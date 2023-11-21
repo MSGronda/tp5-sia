fix: vae backpropagation

ej2.py
@@ -2,11 +2,13 @@ import json
 import random
 from functools import partial
 import numpy as np
+from matplotlib import pyplot as plt
 from perceptron.functions import sigmoid, sigmoid_derivative
 from perceptron.optimizers import ADAM, Momentum
 from perceptron.vae import VAE
 from training_data.emoji import emojis
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 def print_emoji(array):
@@ -19,6 +21,64 @@ def print_emoji(array):
             print(1, end="")
         if i % 22 == 0:
             print()
+
+
+def graph_latent_space(vae, emojis):
+    z_space = []
+    labels = ["🙂", "☹️", "😥", "😂", "😜", "😋", "😝", "😮", "😖"]
+
+    for emoji in emojis:
+        z_space.append(vae.encode(emoji))
+
+    x_values, y_values = zip(*z_space)
+
+    fig = px.scatter(x=x_values, y=y_values, text=labels, labels={'x': 'Mu', 'y': 'Sigma'}, title='2D Scatter Plot')
+
+    fig.update_layout(annotations=[
+        dict(x=x_val, y=y_val, text=label, showarrow=False)
+        for x_val, y_val, label in zip(x_values, y_values, labels)
+    ])
+
+    fig.show()
+
+def draw_emoji(emoji):
+    matrix = emoji.reshape(25, 22)
+    plt.imshow(matrix, cmap='Blues', interpolation='nearest')
+    plt.colorbar()
+    plt.show()
+
+
+def draw_outputs(vae, emojis):
+    for emoji in emojis:
+        draw_emoji(vae.forward_propagation(emoji))
+
+def graph_loss(vae):
+
+    x_values = [i for i in range(len(vae.r_loss))]
+    y_values_list1 = vae.r_loss
+    y_values_list2 = vae.kl_loss
+    y_values_list3 = vae.total_loss
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=x_values, y=y_values_list1, mode='markers', name='Reconstruction Loss', marker=dict(color='red')))
+    fig.add_trace(go.Scatter(x=x_values, y=y_values_list2, mode='markers', name='KL Loss', marker=dict(color='green')))
+    fig.add_trace(go.Scatter(x=x_values, y=y_values_list3, mode='markers', name='Total Loss', marker=dict(color='blue')))
+
+    fig.update_layout(title='Loss vs Epochs',
+                      xaxis=dict(title='Epoch'),
+                      yaxis=dict(title='Loss'))
+    fig.show()
+
+def generate_new_emoji(vae, emojis, start_idx, end_idx, total_steps):
+    z1 = vae.encode(emojis[start_idx])
+    z2 = vae.encode(emojis[end_idx])
+
+    dif = z1 - z2
+
+    for step in range(total_steps + 1):
+        new = vae.decode(z2 + (step/total_steps) * dif)
+        draw_emoji(new)
 
 
 if __name__ == "__main__":
@@ -46,6 +106,7 @@ if __name__ == "__main__":
 
     emojis = [np.array(elem) for elem in emojis]
 
+
     vae = VAE(
         config_json["layer_config"],
         partial(sigmoid, config_json["activation_function_beta"]),
@@ -56,29 +117,17 @@ if __name__ == "__main__":
 
     vae.train(config_json["epochs"], emojis)
 
-    z_space = []
-    for emoji in emojis:
-        z_space.append(vae.encode(emoji))
+    if config_json["graph_latent_space"]:
+        graph_latent_space(vae, emojis)
 
-    x_values, y_values = zip(*z_space)
+    if config_json["draw_outputs"]:
+        draw_outputs(vae, emojis)
 
-    fig = px.scatter(x=x_values, y=y_values, labels={'x': 'X-axis', 'y': 'Y-axis'}, title='2D Scatter Plot')
+    if config_json["graph_loss"]:
+        graph_loss(vae)
 
-    fig.show()
-
-    # z1 = vae.encode(emojis[0])
-    # z2 = vae.encode(emojis[1])
-    #
-    # dif = z1 - z2
-    # total_steps = 5
-    #
-    # for step in range(total_steps):
-    #     new = z2 + (step/total_steps) * dif
-    #
-    #     e = vae.decode(new)
-    #     print_emoji(e)
-    #     print("\n-----------------------------------\n")
-
+    if config_json["generate_new_emoji"]:
+        generate_new_emoji(vae, emojis, config_json["start_idx"], config_json["end_idx"], config_json["steps"])
 
 
 

perceptron/vae.py
@@ -9,7 +9,7 @@ from training_data.font import fonts
 
 
 def reconstruction_loss(generated_output, expected_output):
-    return sum(np.power(generated_output - expected_output, 2))
+    return 0.5 * sum(np.power(generated_output - expected_output, 2))
 
 
 def kl(mean, std):
@@ -29,6 +29,15 @@ def count_error(output, expected_output):
             incorrect_pixels += 1
     return incorrect_pixels
 
+def combine_delta_w(decoder, mid_delta_w, encoder, mid_delta_w2, encoder2):
+    decoder.append(mid_delta_w + mid_delta_w2)
+
+    for a, b in zip(encoder, encoder2):
+        decoder.append(a + b)
+
+    return decoder
+
+
 
 class SamplingLayer:
     def compute_activation(self, prev_input):
@@ -36,10 +45,8 @@ class SamplingLayer:
 
         self.epsilon = np.random.standard_normal(int(prev_input.shape[0] / 2)).reshape(-1, 1)
 
-        # Z         = mu        + sigma * epsilon
-        self.output = self.mean + np.dot(self.std, self.epsilon)
-
-        self.excitement = np.concatenate((self.mean, self.std), axis=0)
+        # Z         = mu        + sigma         *   epsilon
+        self.output = self.mean + np.dot(self.std, self.epsilon)  # Equivalente a multiplicacion de matriz
 
         return self.output
 
@@ -119,9 +126,9 @@ class VAE:
             current = layer.compute_activation(current)
 
         return current
-    def backward_propagation(self, expected_output, generated_output):
-        delta_w = []
 
+    def back_propagate_decoder(self, expected_output, generated_output):
+        delta_w = []
         # = = = = Calculamos el delta W de la primera capa del decoder = = = =
 
         prev_delta = (expected_output - generated_output) * self.derivative_activation_function(self.layers[-1].excitement)
@@ -133,15 +140,10 @@ class VAE:
             delta_w.append(delta.reshape(-1, 1) @ np.transpose(self.layers[idx - 1].output.reshape(-1, 1)))
             prev_delta = delta
 
-        # = = = = Calculamos el delta W de la primera capa del encoder = = = =
+        return delta_w, prev_delta
 
-        # Reparametrization trick (que ni entiendo que pingo hace)
-        mean = np.dot(prev_delta, self.layers[self.latent_idx + 1].weights)
-        std = self.layers[self.latent_idx].epsilon.reshape(-1,) * mean
-        prev_delta = np.concatenate((mean, std), axis=0)
-
-        delta_w.append(prev_delta.reshape(-1, 1) @ np.transpose(self.layers[self.latent_idx - 2].output.reshape(-1, 1)))
-
+    def back_propagate_encoder(self, prev_delta):
+        delta_w = []
         # = = = = Calculamos el delta W de las capas ocultas del encoder = = = =
         for idx in range(self.latent_idx - 2, 0, -1):
             delta =  np.dot(prev_delta, self.layers[idx + 1].weights) * self.derivative_activation_function(self.layers[idx].excitement)
@@ -153,6 +155,38 @@ class VAE:
             delta = np.dot(prev_delta, self.layers[1].weights) * self.derivative_activation_function(self.layers[0].excitement)
             delta_w.append(delta.reshape(-1, 1) @ np.transpose(self.input.reshape(-1, 1)))
 
+        return delta_w
+
+    def backward_propagation(self, expected_output, generated_output):
+
+        # Backpropagation del decoder
+        delta_w_decoder, prev_delta = self.back_propagate_decoder(expected_output, generated_output)
+
+        # Reparametrization trick (que ni entiendo que pingo hace)
+
+        # <<<< Reconstruction >>>>>
+
+        mean = np.dot(prev_delta, self.layers[self.latent_idx + 1].weights) * self.derivative_activation_function(self.layers[self.latent_idx].output)
+        std = self.layers[self.latent_idx].epsilon.reshape(-1,) * mean
+        prev_delta = np.concatenate((mean, std), axis=0)
+
+        mid_delta_w = prev_delta.reshape(-1, 1) @ np.transpose(self.layers[self.latent_idx - 2].output.reshape(-1, 1))
+
+        delta_w_encoder = self.back_propagate_encoder(prev_delta)
+
+        # <<<< Regularization >>>>>
+
+        mean_loss = - self.layers[self.latent_idx].mean
+        std_loss = - 0.5 * (np.exp(self.layers[self.latent_idx].std) - 1)
+        prev_delta = np.concatenate((mean_loss, std_loss), axis=0)
+
+        mid_delta_w2 = prev_delta.reshape(-1, 1) @ np.transpose(self.layers[self.latent_idx - 2].output.reshape(-1, 1))
+
+        delta_w_encoder2 = self.back_propagate_encoder(prev_delta)
+
+        # Combinamos ambos nuevos delta_w
+        delta_w = combine_delta_w(delta_w_decoder, mid_delta_w, delta_w_encoder, mid_delta_w2, delta_w_encoder2)
+
         delta_w.reverse()
 
         return delta_w
@@ -161,6 +195,7 @@ class VAE:
         for idx, layer in enumerate(self.layers):
             if idx != self.latent_idx:
                 layer.update_weights(delta_w.pop(0))
+
     def encode(self, data):
         current = data
         self.input = data
@@ -177,6 +212,10 @@ class VAE:
     def train(self, limit, train_data):
         i = 0
         min_error = float("inf")
+        self.kl_loss = []
+        self.r_loss = []
+        self.total_loss = []
+
         while i < limit:
             error = 0
 
@@ -185,7 +224,16 @@ class VAE:
                 delta_w = self.backward_propagation(elem, result)
                 self.update_all_weights(delta_w)
 
-                error += total_loss(self.layers[self.latent_idx].mean, self.layers[self.latent_idx].std, result, elem)
+
+                kl_loss = kl(self.layers[self.latent_idx].mean, self.layers[self.latent_idx].std)
+                r_loss = reconstruction_loss(result, elem)
+
+                self.kl_loss.append(kl_loss)
+                self.r_loss.append(r_loss)
+
+                error = kl_loss + r_loss
+
+                self.total_loss.append(error)
 
             if error < min_error:
                 min_error = error